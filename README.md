8adb48c611a1571b5246a6644596cd0a09be694d
Adam Cooke <adam@atechmedia.com>
2015-01-31 19:15:59 +0000

    add fallback strings

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Florrick
 
-This is a Rails library which integrates with Active Records and provides some 
+This is a Rails library which integrates with Active Records and provides some
 awesome user-initiated string interpolations for your web apps. For example, have
 you ever needed to allow users to insert their own variables into e-mail templates
 or messages?
@@ -24,9 +24,9 @@ be constructed to allow access to a few methods and a relationship.
 
 ```ruby
 class User < ActiveRecord::Base
-  
+
   belongs_to :country
-  
+
   florrick do
     # Provide access to the following fields. All fields must return either a String,
     # Integer, Fixnum, Date or DateTime object.
@@ -41,13 +41,13 @@ class User < ActiveRecord::Base
     # to be provided for usernames).
     string(:user_name) { username }
   end
-  
+
   # Return the user's full name. This method is included above and any instance methods
   # on the model can be included.
   def full_name
     "#{first_name} #{last_name}"
   end
-  
+
 end
 
 class Country < ActiveRecord::Base
@@ -63,6 +63,19 @@ Once you have configured your models, you can convert a string:
 user    = User.first
 string  = "Hello {{user.full_name}}. Welcome to {{user.country.name}}!"
 Florrick.convert(string, :user => user) #=> "Hello Dave Smith. Welcome to Germany!"
+```
+
+## Fallback
+
+If a string intepolation can't be completed, the default behaviour will just do
+nothing and you'll just see your interpolation in the result.
+
+However, you can add a fallback value to any object which will returned if the
+variable requested doesn't exist or a formatter (see below) is not suitable. This
+is achieved by simply adding your fallback string as shown below:
+
+```ruby
+You live in: {{user.country.name | unknown country}}
 ```
 
 If at any point it cannot determine a value for a given interpolation, it will simply return do nothing.
@@ -108,7 +121,7 @@ Date/timestamps
 
 As well as using the built-in formatters, you can register your own as shown below. You an also use
 this to override any of the built-in formatters. The first argument is the name of the formatter, the
-second is an array of "types" which the formatting can be applied to (String, Numeric, Date or Time). 
+second is an array of "types" which the formatting can be applied to (String, Numeric, Date or Time).
 
 ```ruby
 Florrick::Formatter.add 'add5', [Numeric] do |value|
--- lib/florrick/builder.rb
+++ lib/florrick/builder.rb
@@ -1,8 +1,8 @@
 module Florrick
   class Builder
-    
+
     VALID_EXPORT_TYPES = [String, Numeric, Date, Time]
-    
+
     attr_reader :original_string, :objects
 
     def initialize(string, objects = {})
@@ -12,15 +12,21 @@ module Florrick
 
     def output
       string = @original_string.dup
-      string.gsub(/(\{\{([(\w+)\.]+)\}\})/) do
+      string.gsub(/(\{\{([(\w+)\.]+)(?>\ ?\|\ ?([\w\-\+\ \!\?\[\]\(\)]+))?\}\})/) do
+        original_string = $1
+        fallback_string = $3
         parts = $2.split('.')
         final_string = nil
         previous_object = @objects[parts.shift.to_sym]
-        final_string = $1 if previous_object.nil?
+
+        if previous_object.nil?
+          final_string = fallback_string || original_string
+        end
+
         until final_string
           # get the latest part
           if var = parts.shift
-            # if the previous object is suitable for string interpolation, we can 
+            # if the previous object is suitable for string interpolation, we can
             # pass the var to it and get out a string or an integer.
             if previous_object.respond_to?(:string_interpolation_value_for)
               if previous_object.class.string_interpolation_for?(var)
@@ -33,14 +39,14 @@ module Florrick
                 previous_object = previous_object.send(var)
               else
                 # can't do this :(
-                final_string = $1
+                final_string = fallback_string || original_string
               end
             elsif VALID_EXPORT_TYPES.any? { |t| previous_object.is_a?(t)}
               previous_object = Florrick::Formatter.convert(var, previous_object)
             else
               # does not respond to string_interpolation_value_for and isn't a valid object,
               # we can't do anything here just return the passed value.
-              final_string = $1
+              final_string = fallback_string || original_string
             end
           else
             # we're at the end now, if we can return the previous object, lets do it otherwise
@@ -48,7 +54,7 @@ module Florrick
             if VALID_EXPORT_TYPES.any? { |t| previous_object.is_a?(t)}
               final_string = previous_object.to_s
             else
-              final_string = $1
+              final_string = fallback_string || original_string
             end
           end
         end
